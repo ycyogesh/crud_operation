2ffcebd7c019051d23795e0b2dd643323fe5bd95
ycyogesh <ycyogesh183@gmail.com>
2022-08-23 16:20:27 +0530

    added codes

--- onclick.js
+++ onclick.js
@@ -1,59 +1,52 @@
-var express = require("express")
-var mysql      = require('mysql');
-var cors = require("cors")
-var app = express()
-
+var express = require("express");
+var mysql = require("mysql");
+var cors = require("cors");
+var app = express();
 
 app.use(cors());
 app.use(express.json());
 
-
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'nodejs',
-  password : 'Node@123',
-  database : 'crud'
+  host: "localhost",
+  user: "nodejs",
+  password: "Node@123",
+  database: "crud",
 });
- 
-connection.connect(function(err) {
+
+connection.connect(function (err) {
   if (err) {
-    console.error('error connecting: ' + err.stack);
+    console.error("error connecting: " + err.stack);
     return;
   }
- 
-  console.log('connected as id ' + connection.threadId);
+
+  console.log("connected as id " + connection.threadId);
 });
 
+app.get("/sql", (req, res) => {
+  let sql = "select * from user_message";
 
-app.get("/sql",(req,res)=>{
+  connection.query(sql, (err, result) => {
+    if (err) {
+      res.send("Error");
+    }
+    res.json({ result });
+  });
+});
 
-    let sql = 'select * from user_message';
+app.post("/sql", (req, res) => {
+  let data = req.body;
+  console.log(req);
+  console.log("sssssssss", req.body);
+  let sql = "insert into user_message(name, email, message) values(?,?,?)";
+  connection.query(sql, [data.name, data.email, data.msg], (err, result) => {
+    if (err) {
+      res.send("Error");
+    }
+    console.log("!!!!!!!!!!", result);
+    res.send({ result });
+  });
+});
 
-    connection.query(sql,(err,result)=>{
-        if(err){
-            res.send("Error")
-        }
-        res.json({result})
-    });
-    
-
-})
-
-
-app.post("/sql",(req,res)=>{
-    let data = req.body 
-    console.log(req);
-    console.log('sssssssss', req.body);
-    let sql = 'insert into user_message(name, email, message) values(?,?,?)';
-    connection.query(sql,[data.name, data.email, data.msg],(err,result)=>{
-        if(err){
-            res.send("Error")
-        }
-        console.log('!!!!!!!!!!', result)
-        res.send({result})
-    });
-})
-
-app.listen(3002,()=>{
-    console.log("App Running");
-})
+app.listen(3002, () => {
+  console.log("App Running");
+});
